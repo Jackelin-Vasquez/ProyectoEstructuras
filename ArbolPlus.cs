b30c91be44f7b3e4c902b660eded0272591f2ea1
Jackelin Vásquez <jackelinvasquezguzman@gmail.com>
2026-09-04 23:10:16 -0600

    [3] Metodo mostrar y buscar

--- ArbolPlus.cs
+++ ArbolPlus.cs
@@ -12,4 +12,49 @@ public class ArbolBPlus
         this.maxClaves = orden - 1;
         this.raiz = new NodoBPlus(true);
     }
+
+    // Búsqueda en el árbol B+
+    public bool Buscar(int clave)
+    {
+        return BuscarRec(raiz, clave);
+    }
+
+    private bool BuscarRec(NodoBPlus nodo, int clave)
+    {
+        if (nodo.Hoja)
+        {
+            return nodo.Claves.Contains(clave);
+        }
+
+        int posicion = 0;
+        while (posicion < nodo.Claves.Count && clave >= nodo.Claves[posicion])
+        {
+            posicion++;
+        }
+
+        return BuscarRec(nodo.Hijos[posicion], clave);
+    }
+
+    // Método para mostrar la estrucurra del arbol:p
+    public void Mostrar()
+    {
+        Mostrarrecorrido(raiz, 0);
+    }
+
+    private void Mostrarrecorrido(NodoBPlus nodo, int nivel)
+    {
+        string espacios = new string(' ', nivel * 4);
+        if (nodo.Hoja)
+        {
+            Console.WriteLine($"{espacios}Hoja -> [{string.Join(", ", nodo.Claves)}]");
+        }
+        else
+        {
+            Console.WriteLine($"{espacios}Nodo Interno -> [{string.Join(", ", nodo.Claves)}]");
+            foreach (var hijo in nodo.Hijos)
+            {
+                Mostrarrecorrido(hijo, nivel + 1);
+            }
+        }
+    }
 }
