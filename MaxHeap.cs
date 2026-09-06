4f4f1f1315969370516d0cc0752dce226bbc757b
Jackelin Vásquez <jackelinvasquezguzman@gmail.com>
2026-09-05 18:54:57 -0600

    [7] Max/Min Heap

---
+++ MaxHeap.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+
+public class MaxHeap
+{
+    private List<Libro> heap = new List<Libro>();
+
+    public int Count => heap.Count;
+
+    public void Insertar(Libro valor)
+    {
+        heap.Add(valor);
+        HeapifyUp(heap.Count - 1);
+    }
+
+    private void HeapifyUp(int indice)
+    {
+        while (indice > 0)
+        {
+            int padre = (indice - 1) / 2;
+
+            // si es mayor que el padre sube :D
+            if (heap[indice].CompareTo(heap[padre]) > 0)
+            {
+                Libro temp = heap[indice];
+                heap[indice] = heap[padre];
+                heap[padre] = temp;
+                indice = padre;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+    public Libro ObtenerMaximo()
+    {
+        if (heap.Count == 0) return null;
+        return heap[0];
+    }
+
+    public Libro EliminarMaximo()
+    {
+        if (heap.Count == 0) return null;
+        if (heap.Count == 1)
+        {
+            Libro unico = heap[0];
+            heap.RemoveAt(0);
+            return unico;
+        }
+
+        Libro maximo = heap[0];
+        heap[0] = heap[heap.Count - 1];
+        heap.RemoveAt(heap.Count - 1);
+        HeapifyDown(0);
+
+        return maximo;
+    }
+
+    private void HeapifyDown(int indice)
+    {
+        int cantidad = heap.Count;
+
+        while (true)
+        {
+            int mayor = indice;
+            int hijoIzquierdo = 2 * indice + 1;
+            int hijoDerecho = 2 * indice + 2;
+
+            if (hijoIzquierdo < cantidad && heap[hijoIzquierdo].CompareTo(heap[mayor]) > 0)
+            {
+                mayor = hijoIzquierdo;
+            }
+
+            if (hijoDerecho < cantidad && heap[hijoDerecho].CompareTo(heap[mayor]) > 0)
+            {
+                mayor = hijoDerecho;
+            }
+
+            if (mayor == indice) break;
+
+            Libro temp = heap[indice];
+            heap[indice] = heap[mayor];
+            heap[mayor] = temp;
+            indice = mayor;
+        }
+    }
+
+    public void Mostrar()
+    {
+        foreach (var libro in heap)
+        {
+            Console.WriteLine(libro.ToString());
+        }
+    }
+}
