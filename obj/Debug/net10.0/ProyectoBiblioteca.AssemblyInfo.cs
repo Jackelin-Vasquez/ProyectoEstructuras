b6fc512aae738047c3fe570a5988f5a2f55c34c3
Jackelin Vásquez <jackelinvasquezguzman@gmail.com>
2026-09-07 17:12:38 -0600

    [19] Manual de Usuario

--- obj/Debug/net10.0/ProyectoBiblioteca.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProyectoBiblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoBiblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada0d4357de05242da60e9a7a9e996cb2c971c62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6af08237f7d84f6692b03aaeb95e8b17e24c8a06")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoBiblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoBiblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
